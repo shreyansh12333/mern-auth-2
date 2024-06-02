6143800fe530a46c39d1cce8593ca7af953a1612
shreyansh12333 <shreyanpandey30@gmail.com>
2024-06-02 22:53:12 +0530

    connection to the database

--- Api/index.js
+++ Api/index.js
@@ -1,5 +1,13 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config({ path: "../.env" });
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("mongodb connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 const app = express();
-app.listen(3000, () => {
-  console.log("server listening on port 3000");
-});
