c5caedab22a1271d16a994f0d1bf91e1f1c523d2
shreyansh12333 <shreyanpandey30@gmail.com>
2024-06-03 21:14:31 +0530

    complete signup page functionality

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,31 +1,72 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export default function Signup() {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+    console.log(formData);
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await fetch("/api/user/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data);
+      setLoading(false);
+      if (data.success === false) {
+        setError(true);
+        return;
+      }
+      setError(false);
+    } catch (error) {
+      setLoading(false);
+      setError(true);
+    }
+  };
   return (
     <div className=" max-w-lg mx-auto gap-4  p-3 ">
       <h1 className="font-bold mt-6 text-3xl text-center  ">Sign Up</h1>
-      <form className="flex flex-col gap-4 mt-4">
+      <form className="flex flex-col gap-4 mt-4" onSubmit={handleSubmit}>
         <input
           type="text"
-          id="username"
+          id="name"
           placeholder="username"
           className="p-3 rounded-lg bg-slate-100"
+          onChange={handleChange}
         ></input>
         <input
           type="email"
           id="email"
           placeholder="email"
           className="p-3 rounded-lg bg-slate-100"
+          onChange={handleChange}
         ></input>
         <input
           type="text"
           id="password"
           placeholder="password"
           className="p-3 rounded-lg bg-slate-100"
+          onChange={handleChange}
         ></input>
-        <button className="bg-slate-500 rounded-lg text-center p-3 uppercase text-white">
-          Sign Up
+        <button
+          className="bg-slate-500 rounded-lg text-center p-3 uppercase text-white"
+          disabled={loading}
+        >
+          {loading ? "Loading...." : "Sign up"}
         </button>
         <button className="bg-red-500 rounded-lg text-center p-3 uppercase text-white ">
           continue with google
@@ -37,6 +78,7 @@ export default function Signup() {
           <span className="text-blue-400 ml-1"> Sign in</span>
         </Link>
       </p>
+      <p className="text-red-500 mt-5">{error && "Something went wrong"}</p>
     </div>
   );
 }
