37f8f36cfd6429a8263b94c04f4c33eb07844563
shreyansh12333 <shreyanpandey30@gmail.com>
2024-06-06 13:42:47 +0530

    create-signIn api route

--- Api/controllers/auth.controller.js
+++ Api/controllers/auth.controller.js
@@ -1,6 +1,9 @@
 import User from "../models/models.user.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config({ path: "../.env" });
 
 export const signup = async (req, res, next) => {
   const { name, password, email } = req.body;
@@ -9,6 +12,30 @@ export const signup = async (req, res, next) => {
   try {
     await newUser.save();
     res.status(201).json({ message: "User created successfully" });
+  } catch (error) {
+    next(error);
+  }
+};
+// console.log(process.env.SECRET_TOKEN);
+
+export const signIn = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+
+    if (!validUser) return next(errorHandler(404, "User not found"));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+
+    if (!validPassword) return next(errorHandler(401, "Invalid credentials"));
+    const token = jwt.sign({ id: validUser._id }, process.env.SECRET_TOKEN);
+
+    console.log("value of token is:", token);
+    const { password: hashedPassword, ...rest } = validUser._doc;
+    const expiryDate = new Date(Date.now() + 3600000);
+    res
+      .cookie("access_token", token, { httpOnly: true, expires: expiryDate })
+      .status(200)
+      .json(rest);
   } catch (error) {
     next(error);
   }
--- Api/routes/auth.routes.js
+++ Api/routes/auth.routes.js
@@ -1,5 +1,6 @@
 import express from "express";
-import { signup } from "../controllers/auth.controller.js";
+import { signIn, signup } from "../controllers/auth.controller.js";
 const router = express.Router();
 router.post("/signup", signup);
+router.post("/signin", signIn);
 export default router;
